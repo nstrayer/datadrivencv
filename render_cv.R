21656f2c09ea912d24a3b612a2e44b5798ad0257
Nick Strayer <nick.strayer@gmail.com>
2020-05-14 18:04:07 -0500

    Added a template script that renders both the HTML and PDF at the same time

---
+++ render_cv.R
@@ -0,0 +1,21 @@
+# This script builds both the HTML and PDF versions of your CV
+
+# If you wanted to speed up rendering for googlesheets driven CVs you could use
+# this script to cache a version of the CV_Printer class with data already
+# loaded and load the cached version in the .Rmd instead of re-fetching it twice
+# for the HTML and PDF rendering. This exercise is left to the reader.
+
+# Knit the HTML version
+rmarkdown::render("cv.Rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "cv.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("cv.Rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "cv.pdf")
