b4e0d0f9fd3e4e54d5f182ec247eaa3907f2f525
Nick Strayer <nick.strayer@gmail.com>
2020-05-22 13:11:29 -0500

    The demo CSVs have the same explaining header but I forgot to add the skip to all of the csvs loading

--- inst/templates/CV_printing_functions.R
+++ inst/templates/CV_printing_functions.R
@@ -52,9 +52,9 @@ create_CV_object <-  function(data_location,
     cv$contact_info  <- googlesheets4::read_sheet(data_location, sheet = "contact_info", skip = 1)
   } else {
     # Want to go old-school with csvs?
-    cv$entries_data <- readr::read_csv(paste0(data_location, "entries.csv"))
-    cv$skills       <- readr::read_csv(paste0(data_location, "language_skills.csv"))
-    cv$text_blocks  <- readr::read_csv(paste0(data_location, "text_blocks.csv"))
+    cv$entries_data <- readr::read_csv(paste0(data_location, "entries.csv"), skip = 1)
+    cv$skills       <- readr::read_csv(paste0(data_location, "language_skills.csv"), skip = 1)
+    cv$text_blocks  <- readr::read_csv(paste0(data_location, "text_blocks.csv"), skip = 1)
     cv$contact_info <- readr::read_csv(paste0(data_location, "contact_info.csv"), skip = 1)
   }
 
