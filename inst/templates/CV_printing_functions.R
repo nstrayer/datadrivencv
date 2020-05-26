2b16135005ed996d5b93d766251a439c33c78962
Beat Brüngger <contact.bebru@protonmail.com>
2020-05-26 22:04:02 +0200

    use file.path to construct paths to local files

--- inst/templates/CV_printing_functions.R
+++ inst/templates/CV_printing_functions.R
@@ -46,10 +46,10 @@ create_CV_object <-  function(data_location,
     cv$contact_info  <- googlesheets4::read_sheet(data_location, sheet = "contact_info", skip = 1)
   } else {
     # Want to go old-school with csvs?
-    cv$entries_data <- readr::read_csv(paste0(data_location, "entries.csv"), skip = 1)
-    cv$skills       <- readr::read_csv(paste0(data_location, "language_skills.csv"), skip = 1)
-    cv$text_blocks  <- readr::read_csv(paste0(data_location, "text_blocks.csv"), skip = 1)
-    cv$contact_info <- readr::read_csv(paste0(data_location, "contact_info.csv"), skip = 1)
+    cv$entries_data <- readr::read_csv(file.path(data_location, "entries.csv"), skip = 1)
+    cv$skills       <- readr::read_csv(file.path(data_location, "language_skills.csv"), skip = 1)
+    cv$text_blocks  <- readr::read_csv(file.path(data_location, "text_blocks.csv"), skip = 1)
+    cv$contact_info <- readr::read_csv(file.path(data_location, "contact_info.csv"), skip = 1)
   }
 
 
