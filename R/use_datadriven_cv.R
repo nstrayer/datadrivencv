f55facf5a936c75185ef7d73471940dc6ab17b1a
Nick Strayer <nick.strayer@gmail.com>
2020-05-28 10:57:36 -0500

    Added new argument that allows the updating of specific files instead of all or nothing

--- R/use_datadriven_cv.R
+++ R/use_datadriven_cv.R
@@ -9,19 +9,25 @@
 #' @param data_location Path of the spreadsheets holding all your data. This can
 #'   be either a URL to a google sheet with multiple sheets containing the four
 #'   data types or a path to a folder containing four `.csv`s with the neccesary
-#'   data. See \code{\link{use_csv_data_storage()}} for help setting up these `.csv`s.
+#'   data. See \code{\link{use_csv_data_storage()}} for help setting up these
+#'   `.csv`s.
 #' @param pdf_location What location will the PDF of this CV be hosted at?
 #' @param html_location What location will the HTML version of this CV be hosted
 #'   at?
 #' @param source_location Where is the code to build your CV hosted?
 #' @param open_files Should the added files be opened after creation?
+#' @param which_files What files should be placed? Takes a vector of possible
+#'   values `c("cv.Rmd", "dd_cv.css", "render_cv.R", "CV_printing_functions.R")`
+#'   or `"all"` for everything. This can be used to incrementally update the
+#'   printing functions or CSS without loosing customizations you've made to
+#'   other files.
 #' @param use_network_logo Should logo be an interactive network based on your
 #'   CV data? Note that this uses the function
 #'   \code{\link{build_network_logo()}} so will introduce a dependency on this
 #'   package.
 #'
-#' @return `cv.Rmd`, `dd_cv.css`, `render_cv.R`, and `CV_printing_functions.R` written to the current
-#'   working directory.
+#' @return `cv.Rmd`, `dd_cv.css`, `render_cv.R`, and `CV_printing_functions.R`
+#'   written to the current working directory.
 #'
 #' @examples
 #'
@@ -41,40 +47,53 @@ use_datadriven_cv <- function(full_name = "Sarah Arcos",
                               pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
                               html_location = "nickstrayer.me/datadrivencv/",
                               source_location = "https://github.com/nstrayer/datadrivencv",
+                              which_files = "all",
                               use_network_logo = TRUE,
                               open_files = TRUE){
 
-  # Sets the main Rmd template
-  usethis::use_template(
-    template = "cv.Rmd",
-    package = "datadrivencv",
-    data = list(
-      full_name = full_name,
-      data_location = data_location,
-      pdf_location = pdf_location,
-      html_location = html_location,
-      source_location = source_location,
-      use_network_logo = use_network_logo
-    ),
-    open = open_files
-  )
+  if(which_files == "all"){
+    which_files <- c("cv.Rmd", "dd_cv.css", "render_cv.R", "CV_printing_functions.R")
+  }
 
-  # Place the css as well
-  usethis::use_template(
-    template = "dd_cv.css",
-    package = "datadrivencv",
-    open = open_files
-  )
+  if("cv.Rmd" %in% which_files){
+    # Sets the main Rmd template
+    usethis::use_template(
+      template = "cv.Rmd",
+      package = "datadrivencv",
+      data = list(
+        full_name = full_name,
+        data_location = data_location,
+        pdf_location = pdf_location,
+        html_location = html_location,
+        source_location = source_location,
+        use_network_logo = use_network_logo
+      ),
+      open = open_files
+    )
+  }
 
-  usethis::use_template(
-    template = "render_cv.R",
-    package = "datadrivencv",
-    open = open_files
-  )
+  if("dd_cv.css" %in% which_files){
+    # Place the css as well
+    usethis::use_template(
+      template = "dd_cv.css",
+      package = "datadrivencv",
+      open = open_files
+    )
+  }
 
-  usethis::use_template(
-    template = "CV_printing_functions.R",
-    package = "datadrivencv"
-  )
+  if("render_cv.R" %in% which_files){
+    usethis::use_template(
+      template = "render_cv.R",
+      package = "datadrivencv",
+      open = open_files
+    )
+  }
+
+  if("CV_printing_functions.R" %in% which_files){
+    usethis::use_template(
+      template = "CV_printing_functions.R",
+      package = "datadrivencv"
+    )
+  }
 
 }
