59edf52b4020e135d5f85ba20c05c1d41e2edd27
Nick Strayer <nick.strayer@gmail.com>
2020-06-28 15:02:05 -0500

    Setup the render script to let user know about cache option

--- inst/templates/render_cv.R
+++ inst/templates/render_cv.R
@@ -1,19 +1,30 @@
 # This script builds both the HTML and PDF versions of your CV
 
-# If you wanted to speed up rendering for googlesheets driven CVs you could use
-# this script to cache a version of the CV_Printer class with data already
-# loaded and load the cached version in the .Rmd instead of re-fetching it twice
-# for the HTML and PDF rendering. This exercise is left to the reader.
+# If you want to speed up rendering for googlesheets driven CVs you can cache a
+# version of your data This avoids having to fetch from google sheets twice and
+# will speed up rendering. It will also make things nicer if you have a
+# non-public sheet and want to take care of the authentication in an interactive
+# mode.
+# To use, simply uncomment the following lines and run them once.
+# If you need to update your data delete the "ddcv_cache.rds" file and re-run
+
+# source("CV_printing_functions.R")
+# create_CV_object(
+#   data_location = "{{{data_location}}}",
+#   cache_data = TRUE
+# )
+
+cache_data <- TRUE
 
 # Knit the HTML version
 rmarkdown::render("cv.rmd",
-                  params = list(pdf_mode = FALSE),
+                  params = list(pdf_mode = FALSE, cache_data = cache_data),
                   output_file = "cv.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
 rmarkdown::render("cv.rmd",
-                  params = list(pdf_mode = TRUE),
+                  params = list(pdf_mode = TRUE, cache_data = cache_data),
                   output_file = tmp_html_cv_loc)
 
 # Convert to PDF using Pagedown
